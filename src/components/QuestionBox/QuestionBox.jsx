008206a06eba6c1bb055c8c05732137b4657084e
Marcin Pestka <pestka.m.j@gmail.com>
2022-06-28 22:38:24 +0200

    fix_answers

--- src/components/QuestionBox/QuestionBox.jsx
+++ src/components/QuestionBox/QuestionBox.jsx
@@ -33,13 +33,13 @@ export default function QuestionBox(props) {
                                     onClick={checkAnswer}
                                     class="w-1/3 mx-4 bg-[#8d99ae] hover:bg-[#2b2d42] text-white font-bold py-2 px-4 rounded border border-slate-900 hover:border-indigo-900"
                                 >
-                                    A) {props.answerCorrect}
+                                    A) {props.answer1}
                                 </button>
                                 <button
                                     onClick={checkAnswer}
                                     class="w-1/3 mt-8 mx-4 bg-[#8d99ae] hover:bg-[#2b2d42] text-white font-bold py-2 px-4 rounded border border-slate-900 hover:border-indigo-900"
                                 >
-                                    B) {props.answer1}
+                                    B) {props.answer2}
                                 </button>
                             </div>
 
@@ -48,13 +48,13 @@ export default function QuestionBox(props) {
                                     onClick={checkAnswer}
                                     class="w-1/3 mx-4 bg-[#8d99ae] hover:bg-[#2b2d42] text-white font-bold py-2 px-4 rounded border border-slate-900 hover:border-indigo-900"
                                 >
-                                    C) {props.answer2}
+                                    C) {props.answer3}
                                 </button>
                                 <button
                                     onClick={checkAnswer}
                                     class="w-1/3 mt-8 mx-4 bg-[#8d99ae] hover:bg-[#2b2d42] text-white font-bold py-2 px-4 rounded border border-slate-900 hover:border-indigo-900"
                                 >
-                                    D) {props.answer3}
+                                    D) {props.answer4}
                                 </button>
 
                                 <div className="submit">
--- src/data/Questions.js
+++ src/data/Questions.js
@@ -5,15 +5,18 @@ export const Questions = [
         answerCorrect: 2,
         answer1: 3,
         answer2: 5,
-        answer3: 4
+        answer3: 2,
+        answer4: 7
     },
     {
         id:2,
         question:"x+1=10",
         answerCorrect: 9,
         answer1: 7,
-        answer2: 8,
-        answer3: 10
+        answer2: 9,
+        answer3: 10,
+        answer4: 11
+
     },
     {
         id:3,
@@ -21,7 +24,8 @@ export const Questions = [
         answerCorrect: 0,
         answer1: 11,
         answer2: 5,
-        answer3: 2
+        answer3: 0,
+        answer4: 18
     },
     {
         id:4,
@@ -29,7 +33,8 @@ export const Questions = [
         answerCorrect: 3,
         answer1: 7,
         answer2: 5,
-        answer3: 22
+        answer3: 3,
+        answer4: 10
     },
     {
         id:5,
@@ -37,7 +42,8 @@ export const Questions = [
         answerCorrect: 3,
         answer1: 9,
         answer2: 18,
-        answer3: 5
+        answer3: 5,
+        answer4: 3
     },
     
     ]
