b2d7a0b50959ed8740028c84edf2e8957bded969
Jakub Prach <jakub.prach76@gmail.com>
2022-06-28 20:54:51 +0200

    questions

--- src/components/QuestionBox/QuestionBox.jsx
+++ src/components/QuestionBox/QuestionBox.jsx
@@ -1,8 +1,40 @@
 import React from 'react'
-
 export const QuestionBox = () => {
+
+    const questions  = [
+        {
+            id:1,
+            question:"x+2=4",
+            answer: 2
+    },
+    {
+        id:2,
+        question:"x+1=10",
+        answer: 9
+    },
+    {
+        id:3,
+        question:"x-0=0",
+        answer: 0
+    },
+    {
+        id:4,
+        question:"3x=9",
+        answer: 3
+    },
+    {
+        id:5,
+        question:"15x=5",
+        answer: 3
+    },
+    
+    ]
+    
+
   return (
-    <div>QuestionBox</div>
+    <div className='container'>
+        <h1>{questions[0].question}</h1>
+    </div>
   )
 }
 
