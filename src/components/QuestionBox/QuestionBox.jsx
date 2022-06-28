eae1ee54b0dc77068a2cd7de619d8585313e69fa
Marcin Pestka <pestka.m.j@gmail.com>
2022-06-28 22:34:59 +0200

    answer_only_on_a

--- src/components/QuestionBox/QuestionBox.jsx
+++ src/components/QuestionBox/QuestionBox.jsx
@@ -31,30 +31,30 @@ export default function QuestionBox(props) {
                             <div class="mb-8">
                                 <button
                                     onClick={checkAnswer}
-                                    class="mx-4 bg-[#8d99ae] hover:bg-[#2b2d42] text-white font-bold py-2 px-4 rounded border border-slate-900 hover:border-indigo-900"
+                                    class="w-1/3 mx-4 bg-[#8d99ae] hover:bg-[#2b2d42] text-white font-bold py-2 px-4 rounded border border-slate-900 hover:border-indigo-900"
                                 >
-                                    A) Odpowiedź A
+                                    A) {props.answerCorrect}
                                 </button>
                                 <button
                                     onClick={checkAnswer}
-                                    class="mt-8 mx-4 bg-[#8d99ae] hover:bg-[#2b2d42] text-white font-bold py-2 px-4 rounded border border-slate-900 hover:border-indigo-900"
+                                    class="w-1/3 mt-8 mx-4 bg-[#8d99ae] hover:bg-[#2b2d42] text-white font-bold py-2 px-4 rounded border border-slate-900 hover:border-indigo-900"
                                 >
-                                    B) Odpowiedź B
+                                    B) {props.answer1}
                                 </button>
                             </div>
 
                             <div class="mb-8">
                                 <button
                                     onClick={checkAnswer}
-                                    class="mx-4 bg-[#8d99ae] hover:bg-[#2b2d42] text-white font-bold py-2 px-4 rounded border border-slate-900 hover:border-indigo-900"
+                                    class="w-1/3 mx-4 bg-[#8d99ae] hover:bg-[#2b2d42] text-white font-bold py-2 px-4 rounded border border-slate-900 hover:border-indigo-900"
                                 >
-                                    C) Odpowiedź C
+                                    C) {props.answer2}
                                 </button>
                                 <button
                                     onClick={checkAnswer}
-                                    class="mt-8 mx-4 bg-[#8d99ae] hover:bg-[#2b2d42] text-white font-bold py-2 px-4 rounded border border-slate-900 hover:border-indigo-900"
+                                    class="w-1/3 mt-8 mx-4 bg-[#8d99ae] hover:bg-[#2b2d42] text-white font-bold py-2 px-4 rounded border border-slate-900 hover:border-indigo-900"
                                 >
-                                    D) Odpowiedź D
+                                    D) {props.answer3}
                                 </button>
 
                                 <div className="submit">
--- src/data/Questions.js
+++ src/data/Questions.js
@@ -2,27 +2,42 @@ export const Questions = [
     {
         id:1,
         question:"x+2=4",
-        answer: 2
+        answerCorrect: 2,
+        answer1: 3,
+        answer2: 5,
+        answer3: 4
     },
     {
         id:2,
         question:"x+1=10",
-        answer: 9
+        answerCorrect: 9,
+        answer1: 7,
+        answer2: 8,
+        answer3: 10
     },
     {
         id:3,
         question:"x-0=0",
-        answer: 0
+        answerCorrect: 0,
+        answer1: 11,
+        answer2: 5,
+        answer3: 2
     },
     {
         id:4,
         question:"3x=9",
-        answer: 3
+        answerCorrect: 3,
+        answer1: 7,
+        answer2: 5,
+        answer3: 22
     },
     {
         id:5,
         question:"15x=5",
-        answer: 3
+        answerCorrect: 3,
+        answer1: 9,
+        answer2: 18,
+        answer3: 5
     },
     
     ]
