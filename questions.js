e07f94444baca140701f793c593604bc90f92a8a
Jakub Prach <jakub.prach76@gmail.com>
2022-06-28 20:40:09 +0200

    questions dummy data

---
+++ questions.js
@@ -0,0 +1,28 @@
+export const Questions  = [
+    {
+        id:1,
+        question:"x+2=4",
+        answer: 2
+},
+{
+    id:2,
+    question:"x+1=10",
+    answer: 9
+},
+{
+    id:3,
+    question:"x-0=0",
+    answer: 0
+},
+{
+    id:4,
+    question:"3x=9",
+    answer: 3
+},
+{
+    id:5,
+    question:"15x=5",
+    answer: 3
+},
+
+]
