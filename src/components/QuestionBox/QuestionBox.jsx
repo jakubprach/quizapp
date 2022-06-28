2aeed176a4302598b0081b6d199142db4cf62651
Marcin Pestka <pestka.m.j@gmail.com>
2022-06-28 22:03:13 +0200

    progress_bar_v1

--- src/components/QuestionBox/QuestionBox.jsx
+++ src/components/QuestionBox/QuestionBox.jsx
@@ -1,4 +1,6 @@
-import React from 'react'
+import React from 'react';
+import 'flowbite';
+
 export const QuestionBox = () => {
 
 const questions  = [
@@ -38,10 +40,21 @@ const questions  = [
       }
 
   return (
+    
     <div className='mx-auto w-1/2 container mt-[40px] bg-[#e5e5e5] rounded-[30px] '>
-        <div class="p-8"> 
+
+
+
+
+
+
+        <div class="p-8">
+             
             <h1 class="font-medium leading-tight text-5xl mt-0 mb-2 text-black-600">{questions[number].question}</h1>
         <div class="mt-[40px]">   
+                <div class="w-full bg-gray-200 rounded-full dark:bg-gray-700">
+                <div class="bg-blue-600 h-4 text-xs font-medium text-blue-100 text-center p-0.5 leading-none rounded-full" style={{width: "45%"}}>45%</div>
+                </div>
                 <div class="mb-8">
                     <button onClick={checkAnswer} class="mx-4 bg-[#8d99ae] hover:bg-[#2b2d42] text-white font-bold py-2 px-4 rounded border border-slate-900 hover:border-indigo-900">
                      A) Odpowiedź A
