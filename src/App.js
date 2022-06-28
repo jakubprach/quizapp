c0d15a86b3f3cd41a4ef4fa38b61b5ba3217bb20
Marcin Pestka <pestka.m.j@gmail.com>
2022-06-28 21:12:27 +0200

    component_cleanup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import './App.css';
 import QuestionBox from './components/QuestionBox/QuestionBox';
-import Quiz from "./components/quiz";
 
 function App() {
   return (
     <div className="App">
       <QuestionBox></QuestionBox>
-      <Quiz />
     </div>
   );
 }
--- src/components/QuestionBox/QuestionBox.jsx
+++ src/components/QuestionBox/QuestionBox.jsx
@@ -37,12 +37,31 @@ const questions  = [
     <div className='container'>
         <h1>{questions[number].question}</h1>
         <div className="answer1">
+
         </div>
         <div className="answer2">
 
         </div>
         <div className="answer3">
+        </div>
+        <div class="mt-[40px]">   
+                <div class="mb-8">
+                    <button class="mr-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    A) Odpowiedź A
+                    </button>
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    B) Odpowiedź B
+                    </button>
+                    </div>
 
+                    <div class="mb-8">
+                    <button class="mr-8 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    C) Odpowiedź C
+                    </button>
+                    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+                    D) Odpowiedź D
+                    </button>
+                    </div>
         </div>
     </div>
   )
