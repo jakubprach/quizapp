98ee52e943b1d1d034bdcd7d966c0d9214e3b26e
Marcin Pestka <pestka.m.j@gmail.com>
2022-06-29 09:35:18 +0200

    css_fix

--- src/components/QuestionBox/QuestionBox.jsx
+++ src/components/QuestionBox/QuestionBox.jsx
@@ -15,7 +15,7 @@ export default function QuestionBox(props) {
     useEffect(() => {
         setBttnClass(() => letter => {
             if (Checked == letter) {
-                return('w-1/3mx-4 bg-yellow-500 hover:bg-[#2b2d42] text-white font-bold py-2 px-4 rounded border border-slate-900 hover:border-indigo-900')
+                return('w-1/3 mx-4 bg-yellow-500 hover:bg-[#2b2d42] text-white font-bold py-2 px-4 rounded border border-slate-900 hover:border-indigo-900')
             } else {
                 return('w-1/3 mt-4  mx-4 bg-[#8d99ae] hover:bg-[#2b2d42] text-white font-bold py-2 px-4 rounded border border-slate-900 hover:border-indigo-900')
             }
